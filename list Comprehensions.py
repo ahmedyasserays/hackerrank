81a4ae8a6899ec0d24302fdf9ba9e1a50da50c3b
Ahmed Yasser <37325924+ahmedyasserays@users.noreply.github.com>
2021-07-25 20:37:30 +0200

    Add files via upload

---
+++ list Comprehensions.py
@@ -0,0 +1,15 @@
+x, y, z= 3, 5, 7  # for example
+n = 9
+
+# using the list comprehensions 
+result = [ [i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1)] 
+
+# this gives the same result using for loops
+# result = []
+# for i in range(x+1):
+#     for j in range(y+1):
+#         for k in range(z+1):
+#             if i + j + k != n:
+#                 result.append([i, j, k])
+
+print(result)
