442edcd44bdcb45a459385cd757992e5a97d0af0
Ahmed Yasser <37325924+ahmedyasserays@users.noreply.github.com>
2021-07-25 23:00:48 +0200

    Add files via upload

--- python/list Comprehensions.py
+++ python/list Comprehensions.py
@@ -1,15 +1,16 @@
 x, y, z= 3, 5, 7  # for example
-n = 9
+n = 9  # on hacker rank just input these numbers
 
 # using the list comprehensions 
 result = [ [i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1)] 
+print(result)
 
 # this gives the same result using for loops
-# result = []
-# for i in range(x+1):
-#     for j in range(y+1):
-#         for k in range(z+1):
-#             if i + j + k != n:
-#                 result.append([i, j, k])
+result = []
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            if i + j + k != n:
+                result.append([i, j, k])
 
 print(result)
